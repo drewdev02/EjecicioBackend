ec5c67486d352ec6db94734860b88fde9b115727
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-05-28 15:39:35 -0400

    MOD: corrección menor

--- src/main/java/com/demo/ejeciciobackend/models/User.java
+++ src/main/java/com/demo/ejeciciobackend/models/User.java
@@ -17,6 +17,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String role;
     private String email;
     private String password;
 }
