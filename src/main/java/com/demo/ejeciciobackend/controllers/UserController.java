e31666c2893de5ee54a7b73e1b7cc8e546f0546f
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-05-28 15:39:13 -0400

    ADD: agregado capa service

--- src/main/java/com/demo/ejeciciobackend/controllers/UserController.java
+++ src/main/java/com/demo/ejeciciobackend/controllers/UserController.java
@@ -1,66 +1,57 @@
 package com.demo.ejeciciobackend.controllers;
 
 import com.demo.ejeciciobackend.models.User;
-import com.demo.ejeciciobackend.repository.UserRepository;
+import com.demo.ejeciciobackend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/")
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<Optional<List<User>>> getAllUsers() {
         log.info("Fetching all users");
-        var users = userRepository.findAll();
+        var users = userService.getAllUsers();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         log.info("Fetching user with ID: {}", id);
-        var optionalUser = userRepository.findById(id);
+        var optionalUser = userService.getUserById(id);
         return optionalUser.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping("/")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<Optional<User>> createUser(@RequestBody User user) {
         log.info("Creating a new user: {}", user);
-        var createdUser = userRepository.save(user);
+        var createdUser = userService.createUser(user);
         return ResponseEntity.ok(createdUser);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         log.info("Updating user with ID: {}", id);
-        var optionalUser = userRepository.findById(id);
-        return optionalUser.map(existingUser -> {
-            existingUser.setName(user.getName());
-            existingUser.setEmail(user.getEmail());
-            // Actualizar otros campos según sea necesario
-            User updatedUser = userRepository.save(existingUser);
-            return ResponseEntity.ok(updatedUser);
-        }).orElse(ResponseEntity.notFound().build());
+        var optionalUser = userService.updateUser(id, user);
+        return optionalUser.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
-
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         log.info("Deleting user with ID: {}", id);
-        var optionalUser = userRepository.findById(id);
-        optionalUser.ifPresent(user -> {
-            userRepository.deleteById(id);
-        });
-        return optionalUser.map(user -> ResponseEntity.noContent().build())
-                .orElse(ResponseEntity.notFound().build());
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
-
-
 }
+
