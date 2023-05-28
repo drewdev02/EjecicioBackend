cc928ef37bba1bcf80dcc4ec57e344ba50a92add
Andrey Rodriguez <andrey.rgz.2016@gmail.com>
2023-05-27 22:07:09 -0400

    MOD

--- src/main/java/com/demo/ejeciciobackend/EjecicioBackendApplication.java
+++ src/main/java/com/demo/ejeciciobackend/EjecicioBackendApplication.java
@@ -2,12 +2,16 @@ package com.demo.ejeciciobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.demo.ejeciciobackend.repository")
+@EntityScan("com.demo.ejeciciobackend.models")
 public class EjecicioBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EjecicioBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EjecicioBackendApplication.class, args);
+    }
 
 }
